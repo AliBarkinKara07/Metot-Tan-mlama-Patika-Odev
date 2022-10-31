0a9e52ae77210e80f69d4e5b6dcb81978aa0215b
AliBarkinKara07 <111884407+AliBarkinKara07@users.noreply.github.com>
2022-10-31 16:18:28 +0300

    Add files via upload

---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 7;
+            Console.WriteLine(a + b);
+
+            int sonuc = Topla(a, b);
+            Console.WriteLine(sonuc);
+
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+
+            Console.WriteLine(a+b);
+            int sonuc2 = ornek.ArttırVeTopla(a, b);
+
+        }
+        static int Topla(int deger1, int deger2)
+        {
+            return (deger1 + deger2);
+
+        }
+    }
+    class Metotlar
+    {
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int ArttırVeTopla(int deger1, int deger2)
+        {
+            deger1 += 1;
+            deger2 += 1;
+            return deger1 + deger2;
+        }
+    }
+}
